ff0c7989613992ab927c16c405773b7188443942
Sup456 <44951682+Sup456@users.noreply.github.com>
2020-10-30 14:56:25 +0300

    fix

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -1,13 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExampleTest {
+
     @Test
-    void success_test() {
+    @Tag("test1")
+    void success_test1() {
         String a = "Hello";
         a = Example.addTestPostfix(a);
         assertTrue(a.equals("HelloTest"));
+    }
+
+    @Test
+    @Tag("test2")
+    void success_test2() {
+        String a = "World";
+        a = Example.addTestPostfix(a);
+        assertTrue(a.equals("WorldTest"));
     }
 }
