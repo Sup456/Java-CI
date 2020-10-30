425cd4cc5d426a8879b11a15f75ac8e62d4dc048
Sup456 <44951682+Sup456@users.noreply.github.com>
2020-10-30 14:45:15 +0300

    initial commit

--- src/test/java/org/example/ExampleTest.java
+++ src/test/java/org/example/ExampleTest.java
@@ -5,16 +5,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExampleTest {
-    @Tag("test1")
+
     @Test
+    @Tag("test1")
     void success_test1() {
         String a = "Hello";
         a = Example.addTestPostfix(a);
         assertTrue(a.equals("HelloTest"));
     }
 
-    @Tag("test2")
     @Test
+    @Tag("test2")
     void success_test2() {
         String a = "World";
         a = Example.addTestPostfix(a);
